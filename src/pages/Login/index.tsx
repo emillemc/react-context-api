fb4f15a6afa013e4c5ca28f5a78ca1ad61649d6c
Emille Martins <emillemcosta@gmail.com>
2022-08-23 14:18:47 -0300

    feat(login): organizing components

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,49 +1,6 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { useAuth } from "../../contexts/AuthContext";
+import FormLogin from "../../components/Form/Login";
 
-type FormValues = {
-  email: string;
-  password: string;
-};
 const Login = () => {
-  const { Login } = useAuth();
-
-  const SignupSchema = yup.object().shape({
-    email: yup.string().required(),
-    password: yup.string().required(),
-  });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>({
-    resolver: yupResolver(SignupSchema),
-  });
-
-  const handleLogin = async (data: FormValues) => {
-    await Login(data);
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit(handleLogin)}>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input placeholder="email" type="email" {...register("email")} />
-          {errors.email && <p>{errors.email.message}</p>}
-        </div>
-
-        <div>
-          <label htmlFor="password">Password</label>
-          <input placeholder="password" type="password" {...register("password")} />
-          {errors.password && <p>{errors.password.message}</p>}
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+  return <FormLogin />;
 };
 export default Login;
